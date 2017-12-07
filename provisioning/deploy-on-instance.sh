00b2ad07885195f17d6f1c1461b722d17e30e93f
Ásdís <disa.erna@gmail.com>
2017-12-07 15:51:06 +0000

    bleh

--- provisioning/deploy-on-instance.sh
+++ provisioning/deploy-on-instance.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-GIT_COMMIT=$1
-INSTANCE_ID=$2
+GIT_COMMIT=$(git rev-parse HEAD)
+INSTANCE_ID=$(cat ./ec2_instance/instance-id.txt
 echo "TAG=${GIT_COMMIT}" > ./ec2_instance/.env
 ./update-env.sh $GIT_COMMIT $INSTANCE_ID
